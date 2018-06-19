7876f68053bc956deef921dd5d12020ace64371b
Edsel Caprice <caprice@mail.usf.edu>
2018-06-19 01:34:11 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,6 @@
 def hello_t(array)
-  i = 0
+  if block_given?
+    i = 0
 
   while i < array.length
     yield(array[i])
@@ -7,6 +8,6 @@ def hello_t(array)
   end
 
   array
+  end
 end
-
 # call your method here!
